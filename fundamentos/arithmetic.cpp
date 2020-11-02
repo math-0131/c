2a5510cec7ac9e70dd3a9a36999559b54e66000b
math-0131 <73441307+math-0131@users.noreply.github.com>
2020-11-02 12:15:12 -0300

    Programa criado para compreensão de operadores aritiméticos

---
+++ fundamentos/arithmetic.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+
+int main () {
+
+    
+
+    int x;
+    int y;
+
+    cout << "Enter a number: ";
+    cin >> x;
+
+    cout << "Enter the second number: ";
+    cin >> y;
+    cout << "Result: " << endl;
+
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << x / y << endl;
+    
+
+    return 0;
+}
