fa1464bc935de35c310a01938cd095f3f925c635
math-0131 <73441307+math-0131@users.noreply.github.com>
2020-11-09 22:10:03 -0300

    Programa doWhile

---
+++ fundamentos/doWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main() {
+
+    int number = -1;
+
+    
+
+    do {
+        cout << "Enter a number or -1 to finish: ";
+        cin >> number;
+    } while(number != -1);
+
+return 0;
+}
