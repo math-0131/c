f963059a86bcf63aec4d2c0ce6c78d642d237808
math-0131 <73441307+math-0131@users.noreply.github.com>
2020-11-15 15:01:38 -0300

    Programa de cópia de valores

---
+++ memoryPointerReference/copy.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main() {
+    int a = 2;
+    int b = a; //cópia do valor
+
+    a++;
+    b--;
+
+    cout << a << " " << b << endl;
+
+    return 0;
+}
