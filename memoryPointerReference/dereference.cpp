38d06ecc877df59baa31f051da8b3a4f83b86b08
math-0131 <73441307+math-0131@users.noreply.github.com>
2020-11-15 18:40:04 -0300

    Programa de desreferencia

---
+++ memoryPointerReference/dereference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+
+int main() {
+
+    double pi = 3.14;
+    double& piRef = pi;
+
+    cout << piRef << endl;
+    cout << &pi << endl;
+
+    cout << *&pi << endl;
+
+    return 0;
+}
